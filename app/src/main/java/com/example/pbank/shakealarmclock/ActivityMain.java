786e9087c14be3c45b9c2b932bb86ba4cea20772
it120188tpj <pavel.timofeenko@privatbank.ua>
2016-04-21 14:05:32 +0300

    запуск№7

--- app/src/main/java/com/example/pbank/shakealarmclock/ActivityMain.java
+++ app/src/main/java/com/example/pbank/shakealarmclock/ActivityMain.java
@@ -2,6 +2,7 @@ package com.example.pbank.shakealarmclock;
 
 import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
